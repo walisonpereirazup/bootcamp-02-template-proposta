8c52b248bbef648e7a18858eaaffcae70af54552
Walison Pereira <walison.pereira@zup.com.br>
2020-11-30 16:34:07 -0300

    chore: project setup.

---
+++ src/main/kotlin/br/com/zup/credicardzero/PropostaApplication.kt
@@ -0,0 +1,11 @@
+package br.com.zup.credicardzero
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PropostaApplication
+
+fun main(args: Array<String>) {
+	runApplication<PropostaApplication>(*args)
+}
